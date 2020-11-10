af2a6d8ca619c1f6e0cd6630d70e51e47b761acf
Jorge Sandez <sandez.jorge@gmail.com>
2020-11-10 14:16:08 -0300

    add some tests

--- TP1/src/euclideanTests.c
+++ TP1/src/euclideanTests.c
@@ -2,7 +2,10 @@
 #include <stdio.h>
 
 int main(int argc, char** argv) {
-    printf("mcd entre 10 y 15: %d\n",mcd(10,15));
-    printf("mcm entre 10 y 15: %d\n",mcm(10,15));
+    printf("mcd entre 10 y 15 tiene que ser 5: %s\n",(mcd(10,15) == 5) ? "true":"false");
+    printf("mcd entre 15 y 10 tiene que ser 5: %s\n",(mcd(10,15) == 5) ? "true":"false");
+    printf("mcm entre 10 y 15 tiene que ser 30: %s\n",(mcm(10,15) == 30) ? "true":"false");
+    printf("mcd entre 10001 y 1 tiene que ser 1: %s\n",(mcd(10001,1) == 1) ? "true":"false");
+    printf("mcm entre 10001 y 1 tiene que ser 10001: %s\n",(mcm(10001,1) == 10001) ? "true":"false");
     return 0;
 }

27f3bdd8150dd1af20acdcd0e68a32cf3de7ae24
fmonpelat <facundo.monpelat@gmail.com>
2020-11-11 10:16:25 -0300

    changes

--- TP1/includes/asmEuclidean.h
+++ TP1/includes/asmEuclidean.h
@@ -7,8 +7,8 @@
 #ifndef __ASM_EUCLIDEAN_H__
 #define __ASM_EUCLIDEAN_H__
 
-extern void asmMcd(unsigned int m, unsigned int n);
+extern int asmMcd(unsigned int m, unsigned int n);
 
-extern void asmMcm(unsigned int m, unsigned int n);
+extern int asmMcm(unsigned int m, unsigned int n);
 
 #endif /* __ASM_EUCLIDEAN_H__ */
